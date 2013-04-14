a9686304314dca0ad8bf6f5b7a3840f5452a4ede
angl <luckyan315@qq.com>
2013-04-14 17:53:49 +0800

    update code

---
+++ routes/index.js
@@ -0,0 +1,43 @@
+
+/*
+ * GET home page.
+ */
+
+exports.index = function(req, res){
+  res.render('index', {title: 'Samsung Web SDK'});
+};
+
+exports.user = function(req, res){
+  console.log('user!');
+};
+
+exports.post = function(req, res){
+  console.log('post!');
+
+};
+
+exports.reg = function(req, res){ 
+  console.log('reg!');
+};
+
+exports.doReg = function(req, res){
+  console.log('doReg!');
+};
+
+exports.login = function(req, res){
+  console.log('login');
+};
+
+exports.doLogin = function(req, res){
+  console.log('doLogin');
+};
+
+exports.logout = function(req, res){
+  console.log('logout!');
+};
+
+
+
+
+
+
