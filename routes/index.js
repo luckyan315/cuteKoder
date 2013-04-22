b66859c05235032fca889a205a7d3228aae718a6
angl <luckyan315@qq.com>
2013-04-22 17:20:48 +0800

    update ide view

--- routes/index.js
+++ routes/index.js
@@ -19,44 +19,17 @@ exports.post = function(req, res){
 
 exports.reg = function(req, res){ 
   // console.log('reg!');
-  res.render('reg', {title: 'Home'});
+  res.render('reg', {title: 'Register'});
 
 };
 
 exports.doReg = function(req, res){
-  // console.log('doReg!');
-  var md5 = crypto.createHash('md5');
-  var pw = md5.update(req.body.password).digest('base64');
-
-  var newUser = new User({
-    name: req.body.username,
-    password: pw
-  });
-
-  User.get(newUser.name, function(err, user){
-    if( user ){
-      err = 'User already exists';
-    }
-
-    if( err ){
-      req.flash('error', err);
-      return res.redirect('/reg');
-    }
-
-    newUser.save(function(err){
-      if( err ){
-        req.flash('error', err);
-        return res.redirect('/reg');
-      }
-      req.session.user = newUser;
-      req.flash('success', 'Login Success!');
-      res.redirect('/');
-    });
-  });
+  console.log('doReg!');
 };
 
 exports.login = function(req, res){
-  console.log('login');
+  // console.log('login');
+  res.render('ide', {title: 'ide'});
 };
 
 exports.doLogin = function(req, res){
