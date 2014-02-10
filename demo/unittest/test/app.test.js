07d6252346fcb0adf353d9bcd168951fdce2b1da
angl <luckyan315@qq.com>
2014-02-10 09:54:16 +0800

    update unit test

--- demo/unittest/test/app.test.js
+++ demo/unittest/test/app.test.js
@@ -1,8 +1,10 @@
 //dependant module
+require('should');
+
 var app = require('../../app.js').app;
 var request = require('supertest');
-var should = require('should');
 var isDebugMode = require('../../config').debug;
+var logger = require('../util/log.js');
 
 //consts
 var PORT = 8888;
@@ -17,11 +19,6 @@ describe('BrowserIDE', function(){
         done();
       }
     });
-  });
-
-  after(function(){
-    //delete resources 
-    request(app).get('/delall').expect(200);
   });
   
   describe('Routes', function(){
@@ -52,7 +49,7 @@ describe('BrowserIDE', function(){
       it('should GET "/idx" invalid req ', function(done){
         request(app)
         .get('/idx')
-        .expect(400, done);
+        .expect(404, done);
       });
     });
 
@@ -71,7 +68,6 @@ describe('BrowserIDE', function(){
       };
 
       describe('sign up', function(){
-
         it('should get signup page', function(done){
           request(app)
           .get('/reg')
@@ -79,19 +75,102 @@ describe('BrowserIDE', function(){
         });
         
         it('should create a user', function(done){
+          //need to sign up db and system , maybe take some time...
+          this.timeout(15000);
+        
           request(app)
           .post('/reg')
           .send(user1)
           .expect(200)
-          .end(done);
+          .end(function(err, res){
+              var body = res.res.text;
+              body.should.have.containEql('success_text');
+              done();            
+          });
         });
 
+        it('should not create a user , if username is not alphanumeric', function(done){
+          request(app)
+            .post('/reg')
+            .send({
+              name: '_123abc3',
+              password: '123456',
+              secquestion: 'unittest',
+              secanswer: 'unittest'
+            })
+            .expect(200)
+            .end(function(err, res){
+              var body = res.res.text;
+              body.should.have.containEql('err_msg');
+              done();
+            });
+        });
+
+        it('should not create a user , if username.length is too much', function(done){
+          request(app)
+            .post('/reg')
+            .send({
+              name: '123abc332165465465465465464',
+              password: '123456',
+              secquestion: 'unittest',
+              secanswer: 'unittest'
+            })
+            .expect(200)
+            .end(function(err, res){
+              var body = res.res.text;
+              body.should.have.containEql('err_msg');
+              done();
+            });
+        });
+
+        it('should not create a user , if username is empty', function(done){
+          request(app)
+            .post('/reg')
+            .send({
+              name: '',
+              password: '123456',
+              secquestion: 'unittest',
+              secanswer: 'unittest'
+            })
+            .expect(200)
+            .end(function(err, res){
+              var body = res.res.text;
+              body.should.have.containEql('err_msg');
+              done();
+            });
+        });
+
+
+        it('should not create a user , password is empty', function(done){
+          request(app)
+            .post('/reg')
+            .send({
+              name: 'asdf123',
+              password: '',
+              secquestion: 'unittest',
+              secanswer: 'unittest'
+            })
+            .expect(200)
+            .end(function(err, res){
+              var body = res.res.text;
+              body.should.have.containEql('err_msg');
+              done();
+            });
+        });
+        
+        
         it('should occur error as create twice', function(done){
           request(app)
           .post('/reg')
           .send(user1)
-          .expect(200) // 200 ?
-          .end(done);          
+          .expect(200)
+          .end(function(err, res){
+            if (err) return done(err);
+
+            var body = res.res.text;
+            body.should.have.containEql('err_msg');
+            done();            
+          });
         });
 
       });
@@ -103,23 +182,59 @@ describe('BrowserIDE', function(){
           .expect(200, done);
         });
         
-        it('should post ok', function(done){
+        it('should redirect to /dash if valid input', function(done){
           request(app)
           .post('/login')
           .send({name: user1.name, password: user1.password})
-          .expect(200)
-          .end(done);
+          .expect(302)
+          .end(function(err, res){
+            if (err) return done(err);
+
+            var location = res.res.headers.location;
+            location.should.include('/dash');
+            done();
+          });
         });
+
+        it('should redirect to home page, if invalid username ', function(done){
+          request(app)
+          .post('/login')
+          .send({name: 'asdf1!@#234', password: 'asdf1234'})
+          .expect(302)
+          .end(function(err, res){
+            if (err) return done(err);
+
+            var location = res.res.headers.location;
+            location.should.include('/');
+            done();
+          });
+        });
+
+        it('should redirect to home page, if invalid password ', function(done){
+          request(app)
+            .post('/login')
+            .send({name: user1.name, password: 'asdf1234'})
+            .expect(302)
+            .end(function(err, res){
+              if (err) return done(err);
+
+              var location = res.res.headers.location;
+              location.should.include('/');
+              done();
+            });
+        });
+
       });
 
-      describe('assume login succeed, valid username', function(){
+      describe('login ok', function(){
 
         describe('dashboard', function(){
           it('should get ok', function(done){
             request(app)
               .get('/dash')
               .query({username: user1.name}) /* query info just for unit test */
-              .expect(200, done);
+              .expect(200)
+              .end(done);
           });
         });
 
@@ -129,20 +244,148 @@ describe('BrowserIDE', function(){
 
     //ipsetting need to connect emu, so only available in release mode
     !isDebugMode && describe('Ip', function(){
-      
+      //TODO: need production mode 
     });
 
     describe('Project', function(){
-      
-    });
+      var user1 = {
+        name: 'unittest1',
+        password: '123456',
+        secquestion: 'unittest',
+        secanswer: 'unittest'
+      };
 
-    describe('sign out', function(){
+      it('should not create project , if not logged in', function(done){
+        request(app)
+        .post('/api/ut_basic_project/create?type=basic')
+        .send()
+        .expect(401)
+        .end(function(err, res){
+          done();
+        });        
+      });
+
+      
+      it('should create a web app framework 2.0 project', function(done){
+        request(app)
+        .post('/api/ut_basic_project/create?type=basic&&username=' + user1.name)
+        .send()
+        .expect(200)
+        .end(function(err, res){
+          done();
+        });
+      });
+
+      it('should create a javascript project', function(done){
+        request(app)
+        .post('/api/ut_js_project/create?type=javascript&&username=' + user1.name)
+        .send()
+        .expect(200)
+        .end(function(err, res){
+          done();
+        });
+      });
+
+      it('should create a PNaCL project', function(done){
+        //need to copy many files as create project..., took some time
+        this.timeout(15000);
+
+        request(app)
+        .post('/api/ut_pnacl_project/create?type=pnacl&&username=' + user1.name)
+        .send()
+        .expect(200)
+        .end(function(err, res){
+          done();
+        });
+      });
+      
+      it('should occur error , as input invalid project_type', function(done){
+        request(app)
+        .post('/api/ut_invalid_project/create?type=xxxx&&username=' + user1.name)
+        .send()
+        .expect(405)
+        .end(done);        
+      });
+
+      it('should not create the same project', function(done){
+        request(app)
+        .post('/api/ut_js_project/create?type=javascript&&username=' + user1.name)
+        .send()
+        .expect(500)
+        .end(function(err, res){
+          done();
+        });        
+      });
+
+      it('should check project name available, The name already existed!', function(done){
+        request(app)
+        .post('/api/ut_js_project/check?username=' + user1.name)
+        .send()
+        .expect(500)
+        .end(done);
+      });
+
+      it('should check project, vaild project name ', function(done){
+        request(app)
+        .post('/api/ut_new_js_project/check?username=' + user1.name)
+        .send()
+        .expect(200)
+        .end(done);        
+      });
+      
+      it('should remove the project', function(done){
+        request(app)
+        .post('/api/ut_js_project/remove?username=' + user1.name)
+        .send()
+        .expect(200)
+        .end(done);
+      });
+
+      it('should remove one project twice ok, see detail [Fix: DF131221-00455]', function(done){
+        request(app)
+        .post('/api/ut_js_project/remove?username=' + user1.name)
+        .send()
+        .expect(200)
+        .end(done);
+      });
+
       
     });
 
     describe('404 Page ', function(){
-      
+      it('should get 404 Page', function(done){
+        request(app)
+        .get('/asdfasdf')
+        .expect(404)
+        .end(function(err, res){
+          if (err) return done(err);
+          var body = res.res.text;
+          body.should.containEql('err_detail');
+          done();
+        });
+      });
     });
 
+    describe('Exit unit test', function(){
+      //delete all cases for unittest
+      describe('cleanup', function(){
+        // it('should delete all users', function(done){
+        //     request(app)
+        //       .get('/delall')
+        //       .expect(200)
+        //       .end(done);         
+        // });
+
+        it('should sign out, redirect to home page', function(done){
+          request(app)
+            .get('/logout')
+            .expect(302)
+            .end(done);                   
+        });
+
+      });
+
+    });
+    
   });
 });
