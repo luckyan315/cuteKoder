cb161b752068c9203d176c060e5ca12fa011b4d7
angl <luckyan315@qq.com>
2014-02-13 14:18:18 +0800

    add args demanded (-s , -t , -p)

--- demo/websocket2tcp/proxy.js
+++ demo/websocket2tcp/proxy.js
@@ -1,8 +1,9 @@
 var path = require('path');
 var url = require('url');
 var fs = require('fs');
-var optimist = require('optimist');
-var argv = optimist.argv;
+var argv = require('optimist')
+  .usage('Usage: $0 -s [source_addr] -t [target_addr] -p [static_dir]')
+  .demand(['s','t']).argv;
 var source_addr = argv.s || '127.0.0.1:8888';
 var target_addr = argv.t;
 var source_addr_port = getPort(source_addr);
@@ -26,7 +27,13 @@ function handleRequest(req, res, next){
       return http_error(res, 404, '404 Not Found');
     }
     
-    
+    fs.readFile(filename, 'binary', function(err, file){
+      if (err) return http_error(res, 500, err);
+
+      res.writeHead(200);
+      res.write(file, 'binary');
+      res.end();
+    });
   });
 };
 
