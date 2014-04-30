cab3791be9b58661b6e2d0f2da904e9af6860fa5
luckyan315 <lucky315.an@gmail.com>
2014-05-01 00:45:52 +0800

    add ut module for server

---
+++ demo/chat/test/server.test.js
@@ -0,0 +1,11 @@
+var request = require('supertest');
+var http = require('http');
+var debug = require('debug')('ChatServerTest');
+
+describe('Chat Server', function(){
+
+  it('should work as a static server', function(done){
+        
+  });
+
+});
