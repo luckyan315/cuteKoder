3a8ffabff1c9ecc98109446c99597ea7e9db8c4c
luckyan315 <luckyan315@qq.com>
2014-05-02 20:59:04 +0800

    add static service

--- demo/chat/server.js
+++ demo/chat/server.js
@@ -6,15 +6,22 @@
 
  "use strict";
 
-var http = require('http');
-var debug = require('debug')('ChatServer');
+var express = require('express');
+var debug = require('debug')('Chat:app');
+var config = require('./config');
+var port = config.dev.port;
 
-var port = 3000;
+var app = exports = module.exports = express();
 
-var server = http.createServer(function(res, req){
-  res.end('hello world');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res){
+  res.send('Hello World');
 });
 
-server.listen(port, function(){
-  debug('http server is listening on port %d', port);
-});
+if (process.env.NODE_ENV !== 'test') {
+  app.listen(port, function(){
+    debug('Chatting server is listening port on %d', port);
+  });
+}
+
