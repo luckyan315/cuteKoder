01554dc8b42c5951505551f36d2f4a8f349b0022
luckyan315 <luckyan315@qq.com>
2014-05-02 20:58:07 +0800

    up unit test

--- demo/chat/test/server.test.js
+++ demo/chat/test/server.test.js
@@ -1,11 +1,31 @@
+
+var should = require('should');
 var request = require('supertest');
 var http = require('http');
-var debug = require('debug')('ChatServerTest');
+var debug = require('debug')('Chat:ServerTest');
+
+var app = require('../server.js');
+var config = require('../config');
+var port = config.test.port;
 
 describe('Chat Server', function(){
+  before(function(done){
+    app.listen(port, function(err, result){
+      if (err) debug(err);
+      done(err);
+    });
+  });
+  
+  it('should got 200 status code when visit get /', function(done){
+    request(app)
+      .get('/')
+      .expect(200, done);
+  });
 
   it('should work as a static server', function(done){
-        
+    request(app)
+      .get('/index.html')
+      .expect(200, done);
   });
 
 });
