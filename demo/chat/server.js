c64502397ce982270b89208953b70b3687f050a8
luckyan315 <luckyan315@qq.com>
2014-05-03 20:52:14 +0800

    import socket.io

--- demo/chat/server.js
+++ demo/chat/server.js
@@ -11,7 +11,10 @@ var debug = require('debug')('Chat:app');
 var config = require('./config');
 var port = config.dev.port;
 
-var app = exports = module.exports = express();
+var app = exports.app = express();
+var http = require('http');
+var httpServer = exports.httpServer = http.createServer(app);
+var io = exports.io = require('socket.io')(httpServer);
 
 app.use(express.static(__dirname + '/public'));
 
@@ -20,8 +23,26 @@ app.get('/', function(req, res){
 });
 
 if (process.env.NODE_ENV !== 'test') {
-  app.listen(port, function(){
+  httpServer.listen(port, function(){
     debug('Chatting server is listening port on %d', port);
   });
 }
+
+io.on('connection', function(socket){
+  debug('New client is connected...[socket id]:' + socket.id);
+
+  socket.on('disconnect', function(){
+    debug('A client disconnected...');
+  });
+});
+
+io.on('error', function(err){
+  debug('[socket.io][error] ', err);
+});
+
+process.on('uncaughtException', function(err) {
+  debug(err);
+  debug(err.stack);
+  throw err;
+});
 
