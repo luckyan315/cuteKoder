2bec09436e70a7f9dcf0160ee61be00ac627b089
luckyan315 <lucky315.an@gmail.com>
2014-05-13 17:59:50 +0800

    min refactor master ipc with slave

--- demo/cluster/master.js
+++ demo/cluster/master.js
@@ -1,23 +1,25 @@
 // cluster sample code
 var cluster = require('cluster');
-var http = require('http');
 var nCpus = require('os').cpus().length;
 
-var port = 3000;
+//config master
+cluster.setupMaster({
+  exec: './worker.js',
+  args: ['--use', 'https'],
+  silent: true
+});
 
-if(cluster.isMaster) {
-  for(var i = 0; i < nCpus; ++i) {
-    cluster.fork();
-  }
 
-  cluster.on('exit', function(worker, code, signal){
-    console.log('worker ' + worker.process.pid + ' died');
-  });
-} else {
-  http.createServer(function(req, res){
-    res.writeHead(200);
-    res.end("hello world\n");
-  }).listen(port, function(){
-    console.log('cluster worker: ' + cluster.worker.id + ' listening port on ' + port);
-  });;
+for(var i = 0; i < nCpus; ++i) {
+  cluster.fork();
 }
+
+Object.keys(cluster.workers).forEach(function(id){
+  cluster.workers[id].on('message', function(msg){
+    console.log('worker: ' + id + ' recv msg: ' + msg);
+  });
+});
+
+cluster.on('exit', function(worker, code, signal){
+  console.log('worker ' + worker.process.pid + ' died');
+});
