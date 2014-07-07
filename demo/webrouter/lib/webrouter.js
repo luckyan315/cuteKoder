6de440d1d0b6cec5019b50096a9c6ad7a0a675da
guanglin.agl <guanglin.agl@alibaba-inc.com>
2014-07-07 23:57:27 +0800

    add initial code about webrouter

---
+++ demo/webrouter/lib/webrouter.js
@@ -0,0 +1,13 @@
+/**
+ * Web Router
+ * 
+ * Copyright (C) 2014 guanglin.an (lucky315.an@gmail.com)
+ */
+
+var debug = require('debug')('webrouter:webrouter');
+
+exports = module.exports = createServer;
+
+function createServer(){
+   debug('create server...');
+}
---
+++ demo/webrouter/test/webrouter.test.js
@@ -0,0 +1,10 @@
+var webrouter = require('..');
+
+describe('webrouter', function(){
+
+  it('should call constructor', function(done){
+    var router = new webrouter();
+    done();
+  });
+  
+});
